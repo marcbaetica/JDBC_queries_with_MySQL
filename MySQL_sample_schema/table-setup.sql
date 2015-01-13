15c48d984e60131b180993464fe0a611a170a47c
Marc-Alexandru Baetica <marc.baetica@gmail.com>
2015-01-13 03:21:12 -0500

    fixed grammar

--- MySQL_sample_schema/table-setup.sql
+++ MySQL_sample_schema/table-setup.sql
@@ -5,19 +5,17 @@ use macRulesSchema;
 drop table if exists macRulesTable;
 
 CREATE TABLE `macRulesTable` (
-  'id' int(5) NOT NULL AUTO_INCREMENT,
+  `id` int(5) NOT NULL AUTO_INCREMENT,
   `mac` varchar(17) NOT NULL,
   `block` int(1) DEFAULT NULL,
-  `user_total` int(4) DEFAULT NULL,
-  `total_all` int(8) DEFAULT NULL,
-  `start_time` decimal(10,2) DEFAULT NULL,
+  `usage_limit` int(8) DEFAULT NULL,
   `stop_time` decimal(10,2) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
-INSERT INTO `macRulesTable` ('id',`mac`,`block`,`user_total`,`total_all`, `start_time`, `stop_time`) VALUES (1,'00:1C:B3:09:85:15',0,1230,99999990, 55000.00, 55000.00);
-INSERT INTO `macRulesTable` ('id',`mac`,`block`,`user_total`,`total_all`, `start_time`, `stop_time`) VALUES (2,'00:1C:B3:09:85:16',0,1231,99999991, 55000.00, 75000.00);
-INSERT INTO `macRulesTable` ('id',`mac`,`block`,`user_total`,`total_all`, `start_time`, `stop_time`) VALUES (3,'00:1C:B3:09:85:17',0,1232,99999992, 55000.00, 130000.00);
+INSERT INTO `macRulesTable` (`id`,`mac`,`block`,`usage_limit`,`stop_time`) VALUES (1,'00:1C:B3:09:85:15',0,99999990, 55000.00);
+INSERT INTO `macRulesTable` (`id`,`mac`,`block`,`usage_limit`,`stop_time`) VALUES (2,'00:1C:B3:09:85:16',0,99999991, 75000.00);
+INSERT INTO `macRulesTable` (`id`,`mac`,`block`,`usage_limit`,`stop_time`) VALUES (3,'00:1C:B3:09:85:17',0,99999992, 130000.00);
 
-INSERT INTO `macRulesTable` ('id',`mac`,`block`,`user_total`,`total_all`, `start_time`, `stop_time`) VALUES (4,'00:1C:B3:09:85:18',1,1233,99999993, 55000.00, 120000.00);
-INSERT INTO `macRulesTable` ('id',`mac`,`block`,`user_total`,`total_all`, `start_time`, `stop_time`) VALUES (5,'00:1C:B3:09:85:19',0,1234,99999994, 55000.00, 60000.00);
+INSERT INTO `macRulesTable` (`id`,`mac`,`block`,`usage_limit`,`stop_time`) VALUES (4,'00:1C:B3:09:85:18',1,99999993, 120000.00);
+INSERT INTO `macRulesTable` (`id`,`mac`,`block`,`usage_limit`,`stop_time`) VALUES (5,'00:1C:B3:09:85:19',0,99999994, 60000.00);
